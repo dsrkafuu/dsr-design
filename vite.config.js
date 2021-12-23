8587b26214a1ad657df36c3cba1c1b287c0aa619
dsrkafuu <dsrkafuu@outlook.com>
2021-12-23 23:53:55 +0800

    chore: fix build issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_BASE_URL || '/',
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [vue()],
   resolve: {
     alias: {
